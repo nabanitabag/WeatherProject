a3ce8178222d362db9557dd4442b56731764a576
Nabanita <ashley.makruz@gmail.com>
2021-02-18 14:16:26 +0530

    edits to https

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get("/", function(req, res) {
 
-  https.get("http://api.openweathermap.org/data/2.5/weather?appid=ae293030bc56a7c970bf90945ae88231&q=london");
+  // https.get("http://api.openweathermap.org/data/2.5/weather?q=london);
   res.send("running");
 })
 
