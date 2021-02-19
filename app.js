0a6c15f22bfe7915bb2e30cb3849340509c10d37
Nabanita <ashley.makruz@gmail.com>
2021-02-19 16:38:02 +0530

    add weather, temperature and icon functionality

--- app.js
+++ app.js
@@ -5,10 +5,29 @@ const app = express();
 
 app.get("/", function(req, res) {
 
-  // https.get("http://api.openweathermap.org/data/2.5/weather?q=london);
-  res.send("running");
-})
+  const query = "London";
+  const appKey = "ae293030bc56a7c970bf90945ae88231";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"+london&units=metric&appid="+appKey;
+
+  https.get(url, function(response) {
+    console.log(response.statusCode);
+
+    response.on("data", function(data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const descr = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imgURL = "https://openweathermap.org/img/wn/"+icon+ "@2x.png";
+
+      res.write("<p>The weather of London is currently "+descr+".</p>");
+      res.write("<h1> The temperature in London is "+temp+" degree Celsius. </h> ");
+      res.write("<img src = "+imgURL+">");
+      res.send();
+
+    });
+  });
+});
 
 app.listen(3000, function() {
-  console.log("server running");
+  console.log("server running on 3000");
 });
